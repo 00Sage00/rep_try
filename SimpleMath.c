5fa88f615ce22c66c4cde1e13c5150b68290e8f6
Kajiken <healthyangry@outlook.com>
2025-06-26 00:03:37 +0900

    Added sub method

--- SimpleMath.c
+++ SimpleMath.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 int add(int a,int b);
+int sub(int a,int b);
 
 int main(void){
     int a=9;
     int b=3;
     int sum = add(a,b);
     printf("sum\n");
+    int dif = sub(a,b);
+    printf("dif\n");
 }
 
 int add(int a,int b){
     return a+b;
+}
+
+int sub(int a,int b){
+    return a-b;
 }
